839247faa02c0bc98557f9f378d6a4c18bf659a6
asbjornandersen <heavymaskinen@hotmail.com>
2020-01-29 13:49:02 +0100

    Refactored validator

--- src/java/eu/sig/training/ch04/AccountValidator.java
+++ src/java/eu/sig/training/ch04/AccountValidator.java
@@ -10,14 +10,19 @@ public class AccountValidator {
     public void validate() throws BusinessException {
         int accountNumber = 0;
         for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            accountNumber = accountNumber + (9 - i) * characterValue;
+            accountNumber = addCharValueToNumber(accountNumber, i);
         }
 
         if (!isValidAccountNumber(accountNumber)) {
             throw new BusinessException("Invalid account number!");
         }
+    }
+
+    private int addCharValueToNumber(int accountNumber, int i) {
+        char character = counterAccount.charAt(i);
+        int characterValue = Character.getNumericValue(character);
+        accountNumber = accountNumber + (9 - i) * characterValue;
+        return accountNumber;
     }
 
     private boolean isValidAccountNumber(int sum) {
