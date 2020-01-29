bfd469124303d2432306c5a38834724ee348da06
asbjornandersen <heavymaskinen@hotmail.com>
2020-01-29 14:31:36 +0100

    Refactored BuildAndSendMail

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -1,39 +1,57 @@
 package eu.sig.training.ch05.buildandsendmail;
 
-public class BuildAndSendMail {
-    // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
-        // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
-        // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
-        // Send message
-        m.send(mId, subject, mMessage);
-    }
-    // end::buildAndSendMail[]
+class Recipient {
+    public String firstName, lastName, division;
+}
 
-    @SuppressWarnings("unused")
-    private MailMessage formatMessage(MailFont font, String string) {
+class FormattedMessage {
+    public String message;
+    public MailFont font;
+    public String subject;
+
+    public MailMessage formatMessage() {
         return null;
     }
+
+
+}
+
+class MailMessage {
+
+}
+
+class MailFont {
+
+}
+
+public class BuildAndSendMail {
+    public void buildAndSendMail(MailMan m, Recipient recipient, FormattedMessage message) {
+        String mId = new MailIdCreator(recipient).create();
+        MailMessage mMessage = message.formatMessage();
+        m.send(mId, message.subject, mMessage);
+    }
+
+    @SuppressWarnings("unused")
 
     private class MailMan {
 
         @SuppressWarnings("unused")
-        public void send(String mId, String subject, MailMessage mMessage) {}
+        public void send(String mId, String subject, MailMessage mMessage) {
+        }
 
     }
 
-    private class MailFont {
 
+    private class MailIdCreator {
+        private Recipient recipient;
+
+        public MailIdCreator(Recipient recipient) {
+            this.recipient = recipient;
+        }
+
+        public String create() {
+            return recipient.firstName.charAt(0) + "." + recipient.lastName.substring(0, 7) + "@"
+                    + recipient.division.substring(0, 5) + ".compa.ny";
+        }
     }
-
-    private class MailMessage {
-
-    }
-
 }
