f6d5a5cbcb1c01b0bec39c6dd7361cebd7f1780d
asbjornandersen <heavymaskinen@hotmail.com>
2020-01-29 13:14:55 +0100

    Refactored FlagFactory, upped Java version to 9

--- src/java/eu/sig/training/ch03/FlagFactory.java
+++ src/java/eu/sig/training/ch03/FlagFactory.java
@@ -2,51 +2,97 @@ package eu.sig.training.ch03;
 
 import java.awt.Color;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FlagFactory {
 
+    private Map<Nationality, Flag> flags = Map.of(
+            Nationality.DUTCH, new DutchFlag(),
+            Nationality.GERMAN, new GermanFlag(),
+            Nationality.BELGIAN, new BelgianFlag(),
+            Nationality.FRENCH, new FrenchFlag(),
+            Nationality.ITALIAN, new ItalianFlag(),
+            Nationality.ROMANIA, new RomanianFlag(),
+            Nationality.IRELAND, new IrishFlag(),
+            Nationality.HUNGARIAN, new HungarianFlag(),
+            Nationality.BULGARIAN, new BulgarianFlag(),
+            Nationality.RUSSIA, new RussianFlag()
+    );
+
     // tag::getFlag[]
     public List<Color> getFlagColors(Nationality nationality) {
-        List<Color> result;
-        switch (nationality) {
-        case DUTCH:
-            result = Arrays.asList(Color.RED, Color.WHITE, Color.BLUE);
-            break;
-        case GERMAN:
-            result = Arrays.asList(Color.BLACK, Color.RED, Color.YELLOW);
-            break;
-        case BELGIAN:
-            result = Arrays.asList(Color.BLACK, Color.YELLOW, Color.RED);
-            break;
-        case FRENCH:
-            result = Arrays.asList(Color.BLUE, Color.WHITE, Color.RED);
-            break;
-        case ITALIAN:
-            result = Arrays.asList(Color.GREEN, Color.WHITE, Color.RED);
-            break;
-        case ROMANIA:
-            result = Arrays.asList(Color.BLUE, Color.YELLOW, Color.RED);
-            break;
-        case IRELAND:
-            result = Arrays.asList(Color.GREEN, Color.WHITE, Color.ORANGE);
-            break;
-        case HUNGARIAN:
-            result = Arrays.asList(Color.RED, Color.WHITE, Color.GREEN);
-            break;
-        case BULGARIAN:
-            result = Arrays.asList(Color.WHITE, Color.GREEN, Color.RED);
-            break;
-        case RUSSIA:
-            result = Arrays.asList(Color.WHITE, Color.BLUE, Color.RED);
-            break;
-        case UNCLASSIFIED:
-        default:
-            result = Arrays.asList(Color.GRAY);
-            break;
+        if (flags.containsKey(nationality)) {
+            return flags.get(nationality).getColors();
         }
-        return result;
-    }
-    // end::getFlag[]
 
+        return new UnclassifiedFlag().getColors();
+    }
+
+    class BulgarianFlag extends Flag {
+        public BulgarianFlag() {
+            super(Color.WHITE, Color.GREEN, Color.RED);
+        }
+    }
+
+    class GermanFlag extends Flag {
+        public GermanFlag() {
+            super(Color.BLACK, Color.RED, Color.YELLOW);
+        }
+    }
+
+    class DutchFlag extends Flag {
+        public DutchFlag() {
+            super(Color.RED, Color.WHITE, Color.BLUE);
+        }
+    }
+
+    class BelgianFlag extends Flag {
+        public BelgianFlag() {
+            super(Color.BLACK, Color.YELLOW, Color.RED);
+        }
+    }
+
+    class FrenchFlag extends Flag {
+        public FrenchFlag() {
+            super(Color.BLUE, Color.WHITE, Color.RED);
+        }
+    }
+
+    class ItalianFlag extends Flag {
+        public ItalianFlag() {
+            super(Color.GREEN, Color.WHITE, Color.RED);
+        }
+    }
+
+    class RomanianFlag extends Flag {
+        public RomanianFlag() {
+            super(Color.BLUE, Color.YELLOW, Color.RED);
+        }
+    }
+
+    class IrishFlag extends Flag {
+        public IrishFlag() {
+            super(Color.GREEN, Color.WHITE, Color.ORANGE);
+        }
+    }
+
+    class HungarianFlag extends Flag {
+        public HungarianFlag() {
+            super(Color.RED, Color.WHITE, Color.GREEN);
+        }
+    }
+
+    class RussianFlag extends Flag {
+        public RussianFlag() {
+            super(Color.WHITE, Color.BLUE, Color.RED);
+        }
+    }
+
+    class UnclassifiedFlag extends Flag {
+        public UnclassifiedFlag() {
+            super(Color.GRAY);
+        }
+    }
 }
